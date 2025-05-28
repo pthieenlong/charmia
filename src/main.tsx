c26615396ef469eef890d2171794ccda999662e0
pthieenlong <p.thieenlong.304@gmail.com>
2025-05-28 14:54:08 +0700

    doing on homepage

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Provider from './app/Provider'
-
+import '@/assets/styles/index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider />
--- src/pages/home/layout/Homepage.layout.tsx
+++ src/pages/home/layout/Homepage.layout.tsx
@@ -1,30 +1,55 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/images/react.svg'
-import './App.css'
-
+import { GlobeAsiaAustraliaIcon } from "@heroicons/react/24/outline"
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import workImage from '@/assets/images/work.jpg'
 function Homepage() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[#F2FAFA] flex flex-col items-center">
+        <nav className="flex flex-rows justify-between w-[1280px] bg-white rounded-[2rem] py-[0.25rem] px-[1rem] mt-[2rem]">
+          <div className="flex flex-rows gap-[1rem] items-center">
+            <GlobeAsiaAustraliaIcon className="w-[3.5rem]"/>
+            <p>
+              <span className="text-[#1E88E5]">Skill</span>base
+            </p>
+          </div>
+          <div className="flex gap-[2rem] items-center">
+            <a href="#">
+              Home
+            </a>
+            <a href="#">
+              Find work
+            </a>
+            <a href="#">
+              Find freelancers
+            </a>
+            <a href="#">Log In</a>
+            <a href="#">Sign up</a>
+            <a href="#" className="text-white block px-[1rem] py-[0.5rem] rounded-[2rem] bg-[#1E88E5]">Post a project</a>
+          </div>
+        </nav>
+        <div className="banner">
+          <div className="">
+            <div className="">
+              <h1>
+                Are you looking for <br /> Freelancers ?
+              </h1>
+              <p>
+                Hire Great Freelancers, Fast. Spacelance helps you hire elite freelancers at a moment's notice
+              </p>
+            </div>
+            <div className="">
+              <a href="#">Hire a freelancer</a>
+              <div className="">
+                <input type="text" />
+                <MagnifyingGlassIcon/>
+              </div>
+            </div>
+          </div>
+          <div className="">
+            <img src={workImage} alt="" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Hello world! This is a simple Vite + React setup. 
-      </p>
-    </>
   )
 }
 
